23577aa1a41059e3eec5d999f19c4a7858c8c61d
Matanel Mac <mmgordon.matanel@gmail.com>
2023-05-06 19:33:39 +0300

    set link through main

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,9 @@ import './app.css'
 import 'normalize.css';
 import 'open-props/open-props.min.css';
 import "fluent-svelte/theme.css";
+import logo from './assets/led.svg';
+
+document.querySelector<HTMLLinkElement>('#iconLink')!.href = logo;
 
 import App from './App.svelte'
 
